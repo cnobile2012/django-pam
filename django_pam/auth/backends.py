8c4e5a7fc13d3fd2330b18967b429349cc8119b0
ncarl <ncarl@netapp.com>
2016-04-05 15:17:19 -0400

    Backend now overrides Django's ModelBackend.

--- django_pam/auth/backends.py
+++ django_pam/auth/backends.py
@@ -9,27 +9,49 @@ import pam as pam_base
 from django.contrib.auth import get_user_model
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.backends import ModelBackend
 
 
-class PAMBackend(object):
+class PAMBackend(ModelBackend):
     _pam = pam_base.pam()
 
     def authenticate(self, username=None, password=None, email=None,
                      **extra_fields):
-        PAMUser = get_user_model()
+        """
+        Authenticate using PAM then get the account if it exists else create
+        a new account.
+
+        :param username: The users username. This is a manditory field.
+        :type username: str
+        :param password: The users password. This is a manditory field.
+        :type password: str
+        :param email: The users email address.
+        :type email: str
+        :param extra_fields: Additonal keyword options of any editable field
+                             in the user model.
+        :rtype: The Django user object.
+        """
+        UserModel = get_user_model()
         user = None
 
         if self._pam.authenticate(username, password):
             try:
-                user = PAMUser.objects.get(username=username)
-            except PAMUser.DoesNotExist:
-                user = PAMUser.objects.create_user(
+                user = UserModel._default_manager.get(username=username)
+            except UserModel.DoesNotExist:
+                user = UserModel.objects.create_user(
                     username, email=None, **extra_fields)
 
         return user
 
     def get_user(self, user):
-        PAMUser = get_user_model()
+        """
+        Get the user by either the ``username`` or the ``pk``.
+
+        :param user: The username or pk.
+        :type user: str or int
+        :rtype: The Django user object.
+        """
+        UserModel = get_user_model()
         obj = None
 
         if user.isdigit() and isinstance(int(user), (int, long)):
@@ -42,8 +64,8 @@ class PAMBackend(object):
                               "type {}.").format(type(user)))
 
         try:
-            obj = PAMUser.objects.get(query)
-        except PAMUser.DoesNotExist:
+            obj = UserModel._default_manager.get(query)
+        except UserModel.DoesNotExist:
             pass
 
         return obj
