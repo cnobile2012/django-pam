ac954d5efdf441b82d9c88ec79ce3964ed1c2881
Carl J. Nobile <carl.nobile@gmail.com>
2016-05-02 17:22:51 -0400

    Maybe fixed the settings?

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@
 import sys
 import os
 import django
+from django.conf import settings
 import datetime
 
 # If extensions (or modules to document with autodoc) are in another directory,
@@ -22,7 +23,15 @@ import datetime
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
 #sys.path.insert(0, os.path.abspath('_ext'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'example_site.settings.stage'
+settings.configure(
+    INSTALLED_APPS=[
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django_pam',
+        ],
+    AUTHENTICATION_BACKENDS=['django_pam.auth.backends.PAMBackend'],
+    DEBUG=False,
+    )
 django.setup()
 
 # -- General configuration ------------------------------------------------
